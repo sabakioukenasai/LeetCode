7f8402df55d8c9f810e7adc131d96944366ab5cf
sabakioukenasai <92242735+sabakioukenasai@users.noreply.github.com>
2022-09-28 09:06:15 +0800

    动态规划求解

--- Interview/17/17.09-getKthMagicNumber/getKthMagicNumber.cpp
+++ Interview/17/17.09-getKthMagicNumber/getKthMagicNumber.cpp
@@ -1,63 +1,27 @@
 #include <vector>
 
+using std::min;
 using std::vector;
 
+// 动态规划 0ms > 100%, 6MB > 76.68%
 class Solution{
-private:
-    struct LinkNode{
-        int val;
-        LinkNode *next;
-        LinkNode() : val(0), next(nullptr) {}
-        LinkNode(int x) : val(x), next(nullptr) {}
-        LinkNode(int x, LinkNode *next) : val(x), next(next) {}
-    };
-    
-    int len;
-    int K;
-    LinkNode *head = new LinkNode();
-    LinkNode *rear = nullptr;
-
-    void append(int x) {
-        if (len == 0) {
-            rear = head->next = new LinkNode(x);
-        }
-        else {
-            LinkNode *p = head;
-            while (p != rear && p->next->val > x)
-                p = p->next;
-            if (p == rear) {
-                rear = rear->next = new LinkNode(x);
-            }
-            else {
-                LinkNode *temp = new LinkNode(x, p->next);
-                p->next = temp;
-            }
-        }
-        if (len++ == K) {       // 如果满了K个数，则去掉最大的那个
-            LinkNode *temp = head->next;
-            head->next = temp->next;
-            delete temp;
-        }
-    }
-
-    void yeild(int num, int rest, int lastprod) {
-        append(num);
-        if (rest == 0)
-            return;
-        if (lastprod <= 3)
-            yeild(num * 3, rest - 1, 3);
-        if (lastprod <= 5)
-            yeild(num * 5, rest - 1, 5);
-        if (lastprod <= 7)
-            yeild(num * 7, rest - 1, 7);
-    }
-
 public:
     int getKthMagicNumber(int k) {
-        K = k;
-        len = 0;
-        yeild(1, k / 2, 1);
-
-        return head->next->val;
+        vector<int> dp(k, 1);
+        int p3 = 0, p5 = 0, p7 = 0;
+        for (int i = 1; i < k; ++i)
+        {
+            dp[i] = min(3 * dp[p3], min(5 * dp[p5], 7 * dp[p7]));
+            if (dp[i] == 3 * dp[p3]) {
+                ++p3;
+            } 
+            if (dp[i] == 5 * dp[p5]) {
+                ++p5;
+            }
+            if (dp[i] == 7 * dp[p7]) {
+                ++p7;
+            }
+        }
+        return dp[k - 1];
     }
 };
